7a5d2acfed35c760681f9adad9d6a5e7f3961607
Ilya <ahitkinilyapavlovich@gmail.com>
2022-06-26 20:42:00 +0300

    Added functional for Circle tool

--- client/src/components/ToolBar.js
+++ client/src/components/ToolBar.js
@@ -6,6 +6,7 @@ import canvasState from '../store/canvasState'
 import Rect from '../tools/Rect'
 import Eraser from '../tools/Eraser'
 import Line from '../tools/Line'
+import Circle from '../tools/Circle'
 
 const ToolBar = () => {
 
@@ -18,7 +19,7 @@ const ToolBar = () => {
         <div className='toolbar'>
             <button className='toolbar__btn brush' onClick={() => toolState.setTool(new Brush(canvasState.canvas))} />
             <button className='toolbar__btn rect' onClick={() => toolState.setTool(new Rect(canvasState.canvas))} />
-            <button className='toolbar__btn circle' />
+            <button className='toolbar__btn circle' onClick={() => toolState.setTool(new Circle(canvasState.canvas))} />
             <button className='toolbar__btn eraser' onClick={() => toolState.setTool(new Eraser(canvasState.canvas))} />
             <button className='toolbar__btn line' onClick={() => toolState.setTool(new Line(canvasState.canvas))} />
             <input onChange={e => changeColor(e)} className='toolbar__inputColor' type='color' />
